45712aa07a3c1956a2aa4be938245667fd76a3c3
jobvdnoort <77000749+jobvdnoort@users.noreply.github.com>
2026-07-03 18:40:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
 // js/main.js
-
-// Importeer de kaart-functie
 import { initializeMap } from './map.js';
 
-// Importeer je nieuwe tool uit de 'tools' map
-import { initializeLoggerTool } from './tools/logger.js';
+// Importeer de nieuwe portal tool
+import { initializePortalTool } from './tools/portal.js';
 
 function startApp() {
     console.log("App is aan het opstarten...");
-    
-    // Initialiseer de kaart
     const view = initializeMap("viewDiv");
 
-    // Zodra de kaart klaar is met laden...
     view.when(() => {
         console.log("Kaart is succesvol geladen!");
         
-        // Start hier je tools op en geef de kaart (view) mee:
-        initializeLoggerTool(view);
-        
-        // Straks voeg je hier simpelweg dit toe:
-        // initializeSketchTool(view);
-        // initializeStreetviewTool(view);
+        // Start de portaal-tool en geef de kaart-view mee
+        initializePortalTool(view);
         
     }).catch(error => {
         console.error("Fout bij het laden van de kaart: ", error);
